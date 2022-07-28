6cfab13bd1d9b080fd77083dc4d130c374360551
Hema.v <hema.v@stackroute.in>
2022-07-29 00:12:51 +0530

    Initial commit

---
+++ p4-product-stock/p4-submission.js
@@ -0,0 +1,42 @@
+const products = [
+    {
+        productName: 'Gucci Round Bucklet Belt',
+        price: 400
+    },
+    {
+        productName: 'Gucci Round Bucklet Belt',
+        price: 450
+    },
+    {
+        productName: 'Gucci Round Bucklet Belt',
+        price: 300
+    },
+    {
+        productName: 'Gucci Round Bucklet Belt',
+        price: 420
+    },
+    {
+        productName: 'Smiley T-Shirt',
+        price: 350
+    },
+    {
+        productName: 'Smiley T-Shirt',
+        price: 150
+    },
+    {
+        productName: 'Shinie Nail Paint',
+        price: 100
+    },
+    {
+        productName: 'Shinie Nail Paint',
+        price: 250
+    },
+    {
+        productName: 'Esbeda Wallet',
+        price: 250
+    }
+];
+
+//Find the stock of each product type
+
+//Write code here
---
+++ p5-function-chaining/p5-submission.js
@@ -0,0 +1,41 @@
+const products = [
+    {
+        productName: 'Gucci Round Bucklet Belt',
+        price: 400
+    },
+    {
+        productName: 'Gucci Round Bucklet Belt',
+        price: 450
+    },
+    {
+        productName: 'Gucci Round Bucklet Belt',
+        price: 300
+    },
+    {
+        productName: 'Gucci Round Bucklet Belt',
+        price: 420
+    },
+    {
+        productName: 'Smiley T-Shirt',
+        price: 350
+    },
+    {
+        productName: 'Smiley T-Shirt',
+        price: 150
+    },
+    {
+        productName: 'Shinie Nail Paint',
+        price: 100
+    },
+    {
+        productName: 'Shinie Nail Paint',
+        price: 250
+    },
+    {
+        productName: 'Esbeda Wallet',
+        price: 250
+    }
+];
+
+//Find the stock of each non-premium products using function chaining
+//Write code here
